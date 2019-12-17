a7de2f705a8b5b45d80b18b3433b127dcdf3eedb
Jayant Singh <jayant.1212@gmail.com>
2019-12-17 22:02:00 +0530

    paramiko based ssh and scp utility

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # python3-code
 This repository is intended to have python3 code for various applications
+
+This repository is primarily being developed using Ubuntu 18.04 installed on top of Windows.
+Ubuntu software is available on MS Store.
+
+Getting path of Ubuntu files:
+https://askubuntu.com/questions/759880/where-is-the-ubuntu-file-system-root-directory-in-windows-subsystem-for-linux-an
+
+
+Setting up gitignore:
+Create a gitignore file ~/.gitingore (Use any template from web)
+Create global config for the same: git config --global core.excludesfile ~/.gitignore
+
+
+Typical list of configuration that is required:
+    sudo apt-get update
+    sudo apt install python3-pip
---
+++ utilities/sshConnect.py
@@ -0,0 +1,45 @@
+import paramiko
+from scp import SCPClient
+
+
+class sshClient ():
+    def __init__(self, host, port, user, pwd):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.pwd = pwd
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # yes
+        try:
+            self.ssh.connect(self.host, self.port, self.user, pwd)
+            self.sshConnStatus = True
+        except paramiko.ssh_exception.AuthenticationException:
+            self.sshConnStatus = False
+            print ('failed login for ', self.host)
+
+
+    def executeCommand(self, cmd):
+        stdin, stdout, stderr = self.ssh.exec_command(cmd)
+        output = stdout.read().decode('ascii')
+        return output if output else stderr.read().decode('ascii')    # bytes into unicode
+
+
+    def ScpGetTransport(self, filename):
+        if not self.sshConnStatus:
+            print ('SSH connection not established, no SCP possible')
+            return
+        mscp = SCPClient(self.ssh.get_transport())
+        mscp.get(filename)
+
+
+    def ScpPutTransport(self, filename, filepath):
+        if not self.sshConnStatus:
+            print ('SSH connection not established, no SCP possible')
+            return
+        mscp = SCPClient(self.ssh.get_transport())
+        mscp.put(filename, filepath)
+
+
+    def __del__(self):
+        self.ssh.close()
+
